9e4f8cd0f0130427bcaa7b153a3f71cb4eff125c
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2024-04-10 15:12:51 +0700

    added nicer error message

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,8 @@ use clap::{Parser, Subcommand};
 use pest::Parser as PestParser;
 use std::fs::File;
 use std::io::{Read, Write};
+use pest::error::Error;
+use pest::iterators::Pairs;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -36,11 +38,15 @@ pub(crate) fn run_cli() {
 fn run_build(source: String, output: String) {
     let mut source_buff = String::new();
     File::open(source)
-        .unwrap()
+        .expect("Can't find source file")
         .read_to_string(&mut source_buff)
-        .unwrap();
+        .expect("Failed to read source file");
 
-    let pairs = HexoParser::parse(Rule::file, source_buff.as_str()).unwrap();
+    let pairs = match HexoParser::parse(Rule::file, source_buff.as_str()) {
+        Ok(pairs) => pairs,
+        Err(err) => panic!("Can't parse source file\n{}", err)
+    };
+
 
     let ast = ast::parse_ast(String::from("java_file"), pairs);
     let cst = cst::parse_cst(ast);
