ec8f656aec2fe84bcb8b07b2ce2c15abc51d83d9
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2024-04-11 20:59:00 +0700

    removed unused code

--- src/render.rs
+++ src/render.rs
@@ -6,9 +6,7 @@ pub(crate) fn render_cst(cst_file: CstFile) -> Vec<u8> {
     let emits = cst_file.emits();
     let mut buf = Vec::new();
     emits.iter().for_each(|emit| {
-        let atoms = emit
-            .atoms
-            .iter()
+        let atoms = emit.atoms.iter()
             .map(|atom| eval_atom(atom))
             .collect::<Vec<_>>();
         buf.extend(atoms.iter().flatten());
@@ -17,35 +15,9 @@ pub(crate) fn render_cst(cst_file: CstFile) -> Vec<u8> {
     buf
 }
 
-pub(crate) fn build_const_map(constants: Vec<&CstStatementConst>) -> HashMap<String, Vec<CstAtom>> {
-    let bindings: Vec<_> = constants
-        .iter()
-        .map(|cst_const| {
-            let name = cst_const.name.to_string();
-            let value = &cst_const.atoms;
-            (name, value)
-        })
-        .collect();
-
-    let buf = HashMap::new();
-    bindings.into_iter().fold(buf, |mut acc, (name, value)| {
-        acc.insert(name, value.to_vec());
-        acc
-    })
-}
-
 pub(crate) fn eval_atom(atom: &CstAtom) -> Vec<u8> {
     match atom {
         CstAtom::Resolved { value } => value.to_vec(),
-        _ => panic!("unresolved atom are not supported in renderer"),
-    }
-}
-
-pub(crate) fn resolve_atom(atom: &CstAtom) -> Vec<CstAtom> {
-    match atom {
-        CstAtom::Resolved { value } => vec![CstAtom::Resolved {
-            value: value.to_vec(),
-        }],
         _ => panic!("unresolved atom are not supported in renderer"),
     }
 }
