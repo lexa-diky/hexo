56e007e20c1e4c6c726261cb9af65d4d62c0a0b2
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2024-05-13 16:48:12 +0700

    added function bindings model

--- src/compiler/rst/compilation_context.rs
+++ src/compiler/rst/compilation_context.rs
@@ -1,4 +1,4 @@
-use crate::compiler::cst::CstFile;
+use crate::compiler::cst::{CstConstantStatement, CstEmitStatement, CstFile, CstFunctionStatement};
 use std::collections::HashMap;
 use std::path::PathBuf;
 
@@ -9,9 +9,16 @@ pub(crate) struct ConstantBinding {
     pub(crate) byte_buffer: ByteBuffer,
 }
 
+pub(crate) struct FunctionBinding {
+    pub(crate) name: String,
+    pub(crate) byte_buffer: ByteBuffer,
+    pub(crate) sub_context: CompilationContext,
+}
+
 pub(crate) struct CompilationContext {
     self_path: PathBuf,
     constant_table: HashMap<String, ConstantBinding>,
+    function_table: HashMap<String, FunctionBinding>,
 }
 
 impl CompilationContext {
@@ -19,6 +26,7 @@ impl CompilationContext {
         return CompilationContext {
             self_path: path.clone(),
             constant_table: HashMap::new(),
+            function_table: HashMap::new(),
         };
     }
 
@@ -32,5 +40,17 @@ impl CompilationContext {
 
     pub(crate) fn get_constant(&self, name: &String) -> Option<&ConstantBinding> {
         return self.constant_table.get(name);
+    }
+
+    pub(crate) fn bind_function(&mut self, function: FunctionBinding) {
+        self.function_table.insert(function.name.clone(), function);
+    }
+
+    pub(crate) fn has_function(&self, name: &String) -> bool {
+        return self.function_table.contains_key(name);
+    }
+
+    pub(crate) fn get_function(&self, name: &String) -> Option<&FunctionBinding> {
+        return self.function_table.get(name);
     }
 }
