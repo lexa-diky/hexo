ee9490ae08b6b0be5907d96d68d906a9f50fed7e
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2024-06-28 17:02:46 +0700

    improved error messages

--- src/cli/error.rs
+++ src/cli/error.rs
@@ -11,10 +11,10 @@ pub(crate) enum Error {
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::UnknownCommand => write!(f, "Unknown command"),
-            Error::FileWatcher(e) => write!(f, "File watching error: {}", e),
-            Error::CantCrateOutputFile(e) => write!(f, "Can't create output file: {}", e),
-            Error::Compilation(e) => write!(f, "Compilation error: {}", e),
+            Error::UnknownCommand => write!(f, "Unknown command, please run hexo -h for help"),
+            Error::FileWatcher(e) => write!(f, "File watching error:\n{}", e),
+            Error::CantCrateOutputFile(e) => write!(f, "Can't create output file:\n{}", e),
+            Error::Compilation(e) => write!(f, "Compilation error:\n{}", e),
         }
     }
 }
